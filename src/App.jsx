10525dbeb2bc811a38f2b24735972781ab2f042f
Ravy Carvalho <ravyestudandoreact@gmail.com>
2023-09-21 19:48:40 -0300

    07_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import React from "react";
 //import HookState03 from './components/UseState_03'
 //import HookState04 from './components/UseState_04'
 //import HookState05 from './components/UseState_05'
-import HookState06 from './components/UseState_06'
+//import HookState06 from './components/UseState_06'
+import HookState07 from './components/UseState_07'
 
 export default function App() {
 
@@ -17,8 +18,10 @@ export default function App() {
                {/*<HookState02 />*/}
                {/*<HookState03 />*/}
                {/*<HookState04 />*/}
+               {/*<HookState05 />*/}
+               {/*<HookState06 />*/}
 
-               <HookState06 />
+                  <HookState07 />
         </>
     )
 }
